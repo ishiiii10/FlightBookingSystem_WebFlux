7d86a01cefdfc955d367321061a6ce4c1b130700
Ishika <22052461@kiit.ac.in>
2025-11-24 00:12:15 +0530

    WebFlux FlightApp Started

---
+++ Webflux_FlightBooking/src/main/java/com/FlightBooking/WebfluxFlightBookingApplication.java
@@ -0,0 +1,13 @@
+package com.FlightBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxFlightBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxFlightBookingApplication.class, args);
+	}
+
+}
