30a0e93f0e0b5df06c51fa757c63883d33fc75c3
Ishika <22052461@kiit.ac.in>
2025-11-24 17:58:12 +0530

    interface for implementation of services updated

--- Webflux_FlightBooking/src/main/java/com/FlightBooking/repository/BookingRepository.java
+++ Webflux_FlightBooking/src/main/java/com/FlightBooking/repository/BookingRepository.java
@@ -1,5 +1,18 @@
 package com.FlightBooking.repository;
 
-public interface BookingRepository {
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+import com.FlightBooking.entity.Booking;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface BookingRepository extends ReactiveMongoRepository<Booking, String> {
+
+    Flux<Booking> findByUserEmail(String userEmail);
+    
+ // Explicit method: get booking by PNR
+    Mono<Booking> findByPnr(String pnr);
 }
